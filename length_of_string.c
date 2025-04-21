65c790165327209a4dde51c790b7afc9c31b2f6f
Mmsshakil <softhandshakil@gmail.com>
2025-04-22 03:48:42 +0600

    length of string

---
+++ length_of_string.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    char s[50];
+    scanf("%s", &s);
+    // printf("%s\n", s);
+
+    int count = 0;
+    for (int i = 0; i != '\0'; i++)
+    {
+        printf("%c", s[i]);
+        count++;
+    }
+    // int i = 0;
+    // while (i != '\0')
+    // {
+    //     count++;
+    //     i++;
+    // }
+
+    // printf("%d", count);
+
+    return 0;
+}
