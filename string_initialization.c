7f710f746908f13dbf4b1ee42577eda4aa966673
Mmsshakil <softhandshakil@gmail.com>
2025-04-22 01:42:36 +0600

    git initialization done

--- string_initialization.c
+++ string_initialization.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 int main()
 {
+    char s[7] = {'S', 'H', 'A', 'K', 'I', 'L'}; // here 7 because we need to give space for '\0' or null
+
     char str[20] = "shahriar shakil";
     printf("%s", str);
 
